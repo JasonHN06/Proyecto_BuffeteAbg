4123c6a3619f9590a959693a463fedafa525d5ba
Jason Madrid <jasonhnn6@gmail.com>
2025-08-20 14:40:27 -0600

    Primer commit

---
+++ js/testimonios.js
@@ -0,0 +1,5 @@
+document.getElementById("form-testimonio").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("¡Gracias por tu testimonio! Será revisado y publicado próximamente.");
+    this.reset();
+});
